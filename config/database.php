daf0e201733d00d70bb2b6ae978d3d7c5bf14866
salmab123 <salmabouizmoun@gmail.com>
2021-05-12 13:05:25 +0000

    fixe typo

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'syage2'),
+            'database' => env('DB_DATABASE', 'stage2'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
